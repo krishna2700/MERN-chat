0b4d29b8e5b2edaf1ab34c5774ba17a65fdf7b56
krishna2700 <krishnaruparelia0207@gmail.com>
2025-04-07 21:03:18 +0530

    login API , dummy token and genrate token function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -29,3 +29,34 @@ userRouter.post("/register", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// Login
+userRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        user: {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
